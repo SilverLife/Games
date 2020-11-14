83be22cc04936f49f872db484f4f98b1d22eda14
Andre <Silver_life@mail.ru>
2020-11-14 11:36:32 +0300

    Cosmetic

--- Socoban/Object/Player.h
+++ Socoban/Object/Player.h
@@ -16,22 +16,6 @@ namespace Socoban
 			Player(Point pos)
 				: FieldData::FieldObjectWithPos(pos, '#', Shared::ConsoleHelper::Color::Yellow)
 			{}
-
-			//void OnRemove() override
-			//{
-			//}
-			//
-			//void OnTick(int tick_num) override
-			//{
-			//}
-			//
-			//int OnActiveIntersect(FieldObject* intersected_object) override
-			//{
-			//}
-			//
-			//int OnPassiveIntersect(FieldObject* intersected_object) override
-			//{
-			//}
 		};
 	}
 }
